9c800a058131370ea3bcd6384e63b3901a0e5d42
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-11 18:21:35 +0800

    Implement JwtUtil tool for generating JWTs

---
+++ src/main/java/dev/ailuruslabs/ohmyrest/security/JwtUtil.java
@@ -0,0 +1,37 @@
+package dev.ailuruslabs.ohmyrest.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+class JwtUtil {
+
+    private final Key key;
+    private final long expirationInMs;
+
+    JwtUtil(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration-ms}") long expirationInMs) {
+        this.expirationInMs = expirationInMs;
+
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(Authentication auth) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expirationInMs);
+
+        return Jwts.builder()
+            .subject(auth.getName())
+            .issuedAt(now)
+            .expiration(expiryDate)
+            .signWith(key)
+            .compact();
+    }
+
+}
